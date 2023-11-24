452d6544ee9d6e128ab536ae4d20b246d704ad4c
Enzo Delmachio <enzo.dlmc@gmail.com>
2023-11-24 19:59:28 -0300

    Update CSS

--- index.js
+++ index.js
@@ -4,25 +4,15 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
-const db = require('./db/db_conect');
-const User = require('./model/user');
+
 const UserControl = require('./control/user_control');
 
 app.use(express.json());
-
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(UserControl);
 
-//CONFIGURAÇÃO DO CSS
-//app.use(express.static('paginas'));
-const htmlPath = path.join(__dirname,'../view', 'html');
-const cssPath = path.join(__dirname,'../view', 'css');
-app.use('/view/html', express.static(htmlPath));
-app.use('/view/css', express.static(cssPath, { 
-  setHeaders: (res, path) => {
-    res.setHeader('Content-Type', 'text/css');
-  },
-  }));
+app.use(express.static(__dirname + '/view'));
 
 //app.use(router);
 
