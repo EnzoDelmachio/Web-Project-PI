dcd4602dea13256520219c6fc52cdb0e5cc12a3b
Enzo Delmachio <enzo.dlmc@gmail.com>
2023-12-01 19:49:11 -0300

    db

--- db/db_conect.js
+++ db/db_conect.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("project", "root", "1v4v-u2us-1jav",{
+const sequelize = new Sequelize("project", "root", "INSIRA SUA SENHA AQUI",{
     host:'localhost',
     dialect: 'mysql'
 });
